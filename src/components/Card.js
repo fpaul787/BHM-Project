c7d98871108a43e0d754ef562ea520444b44f3bc
Frantz <fpaul787@gmail.com>
2020-03-08 16:09:42 -0400

    added default if property is false

--- src/components/Card.js
+++ src/components/Card.js
@@ -3,7 +3,19 @@ import PropTypes from 'prop-types'
 
 
 const Card = ({property}) => {
-    const {index, name, picture} = property
+    var index
+    var name
+    var picture
+    
+    if(property == null){
+        index = -1
+        name = "Test Image"
+        picture = "https://projectphotos.blob.core.windows.net/photos/testImage.jpg"
+    }else{
+        index = property.index
+        name = property.name
+        picture = property.picture
+    }
     
     return(        
         <div id={`card-${index}`} className="card"> 
