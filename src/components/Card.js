4370e1de66415596bfd350e2afc7876e87a6f42f
Frantz <fpaul787@gmail.com>
2020-03-21 23:27:25 -0400

    made changes with css

--- src/components/Card.js
+++ src/components/Card.js
@@ -2,42 +2,46 @@ import React from 'react'
 import PropTypes from 'prop-types'
 
 
-const Card = ({property}) => {
+const Card = ({ property }) => {
     var index   // index of property in json file
     var name    // name of property    
     var picture // link to picture for property
     var bio     // bio for property
     var years   // (BirthDate - DateofDeath)
-    
-    
-    
-    if(property == null){
+
+
+
+    if (property == null) {
         index = -1
         name = "Test Image"
         picture = "https://projectphotos.blob.core.windows.net/photos/testImage.jpg"
-    }else{
+    } else {
         index = property.index
         name = property.name
         picture = property.picture
         bio = property.bio
         years = property.years
     }
-    
+
     // in order to get the right amount of cards to show up,
     // everytime new item is added to json file, please 
     // change for loop in App.scss 
-    return(      
-        <React.Fragment> 
-        <div id={`card-${index}`} className="card"> 
-            <img src={picture} alt={name}/>
-            <h2 className="name">{name}</h2>
-            <h4>{years}</h4>
-            {
-                bio.map(b => <h3 className="bio">{b}</h3>)
-            }
-            <a target="_blank" rel="noopener noreferrer" className="more-info" href={property.link}>Click for more info</a>            
-        </div>       
-        </React.Fragment> 
+    return (
+        <React.Fragment>
+            <div id={`card-${index}`} className="card">
+                <img src={picture} alt={name} />
+                <h2 className="name">{name}</h2>
+
+
+                <h4>{years}</h4>
+                {
+                    bio.map(b => <h3 className="bio">{b}</h3>)
+                }
+                <a target="_blank" rel="noopener noreferrer" className="more-info" href={property.link}>Click for more info</a>
+
+            </div>
+
+        </React.Fragment>
     )
 }
 Card.propTypes = {
