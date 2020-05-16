13b92ff2d1a3068566ac80da2ddfbdb7c589e4f9
Frantz Paul <fpaul787@gmail.com>
2020-05-16 22:45:24 +0000

    Update Navbar.js

--- src/components/layout/Navbar/Navbar.js
+++ src/components/layout/Navbar/Navbar.js
@@ -3,7 +3,7 @@ import React from 'react'
 
 const Navbar = () => {
     return (
-        <div style={{color: 'white', textAlign: 'center'}}>
+        <div style={{color: 'white', textAlign: 'center', paddingRight: '10'}}>
             <h3>Home</h3>
         </div>
     )
